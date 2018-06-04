a2be7309c3b355ee89eb4d5d9f24592831df0444
Yevhen Tokarenko <ytok@isd.dp.ua>
2018-06-04 16:03:11 +0300

    some cosmetic changes

--- lib/main.rb
+++ lib/main.rb
@@ -1,34 +1,33 @@
+#comment
 class Cb
   attr_accessor :store
   @hint_available = true
   def initialize
-    @@store =[]
     @attempts = 5
     @hint_available = true
     @code = []
-    @b
+    @b = []
   end
 
   def generator
-    @code = []
-    @@store = 4.times {@code.push(rand(1..6))}
-    p @code
+    code = []
+    4.times { code.push(rand(1..6)) }
   end
 
   def guess
-    p "enter code"
-    attempt = gets.to_i.digits.reverse
+    p 'enter code'
+    gets.to_i.digits.reverse
   end
 
   def compare(generated, guess)
-    arr = Array.new(4, " ")
+    arr = Array.new(4, ' ')
     generated.zip(guess).each_with_index do |elements_by_their_place, index|
       if elements_by_their_place.first == elements_by_their_place.last
-        arr[index] = "+"
+        arr[index] = '+'
       end
       generated.each_with_index do |_variable, ind|
-        if arr[index] == " " && elements_by_their_place.last == generated[ind]
-          arr[index] = "-"
+        if arr[index] == ' ' && elements_by_their_place.last == generated[ind]
+          arr[index] = '-'
         end
       end
     end
@@ -43,7 +42,7 @@ class Cb
     if @attempts > 0
       puts "Number of attempts left: #{@attempts}"
     else
-      puts "You lost a game"
+      puts 'You lost a game'
       lost
     end
   end
@@ -55,106 +54,108 @@ class Cb
 3 - exit"
     num = gets.to_i
     case num
-      when 1
-        @hint_available = true
-        @attempts = 5
-        new_game
-      when 2
-        view_results
-      when 3
-        exit
-      else
-        puts "Please enter correct number"
+    when 1
+      @hint_available = true
+      @attempts = 5
+      new_game
+    when 2
+      view_results
+    when 3
+      exit
+    else
+      puts 'Please enter correct number'
     end
   end
 
   def write_results
-    puts "Enter your name"
+    puts 'Enter your name'
     username = gets
-    results = "results.txt"
+    results = 'results.txt'
     File.open(results, 'a') { |file| file.write("#{username} finished game with #{@attempts -1} left \n") }
   end
 
-    def view_results
-        File.open("results.txt", "r") do |f|
-            f.each_line do |line|
-                puts line
-            end
-        end
+  def view_results
+    File.open('results.txt', 'r') do |f|
+      f.each_line do |line|
+        puts line
+      end
     end
+  end
 
-    def win
-        puts "Please select:
+  def win
+    puts "Please select:
 1 - Play again
 2 - View results
 3 - Write your result
 4 - exit"
-        num = gets.to_i
-        case num
-            when 1
-                @hint_available = true
-                @attempts = 5
-                new_game
-            when 2
-                view_results
-            when 3
-                write_results
-            when 4
-                exit
-            else
-                puts "Please enter correct number"
-            end
+    num = gets.to_i
+    case num
+    when 1
+      @hint_available = true
+      @attempts = 5
+      new_game
+    when 2
+      view_results
+    when 3
+      write_results
+    when 4
+      exit
+    else
+      puts 'Please enter correct number'
     end
-    def win?(compared)
-        arr = ["+", "+", "+","+"]
-        if compared == arr 
-            puts "You win"  
-            win  
-        end
-    end
+  end
 
-    def hint?
-        if @hint_available && @b == nil
-           @b = [" ", " ", " ", " "] 
-           hint(@code, @b)
-           @hint_available = false
-        elsif @hint_available
-           hint(@code, @b)
-           @hint_available = false
-           p @hint_available
-        else p "You dont have hints"
-        end
+  def win?(compared)
+    arr = ['+', '+', '+', '+']
+    if compared == arr
+      puts 'You win'
+      win
     end
+  end
 
-    def hint(generated_code, compared_array)
-        loop do
-            @hint_index = generated_code[rand(0..3)]
-            @hint_value = generated_code[@hint_index]
-            break if compared_array[@hint_index] != "+"
-        end
-        puts "Number #{@hint_value} is on position #{@hint_index + 1}"  
+  def hint?
+    if @hint_available && @b == nil
+      @b = [' ', ' ', ' ', ' ']
+      hint(@code, @b)
+      @hint_available = false
+    elsif @hint_available
+      hint(@code, @b)
+      @hint_available = false
+      p @hint_available
+    else
+      p 'You dont have hints'
     end
+  end
 
-    def new_game
-        p @code = generator
-        loop do
-            puts "1 - Enter code
+  def hint(generated_code, compared_array)
+    loop do
+      @hint_index = generated_code[rand(0..3)]
+      @hint_value = generated_code[@hint_index]
+      break if compared_array[@hint_index] != "+"
+    end
+    puts "Number #{@hint_value} is on position #{@hint_index + 1}"  
+  end
+
+  def new_game
+    p @code = generator
+    loop do
+      puts "1 - Enter code
 2 - Get a hint"
-            choice = gets.to_i
-            case choice
-                when 1
-                @b = compare(@code, guess)
-                p @b
-                p win?(@b)
-                p attempt_used
-                p lost?
-                when 2
-                hint?
-                else
-                puts "Please enter 1 or 2"
-            end
-        end
+      choice = gets.to_i
+      case choice
+      when 1
+        @b = compare(@code, guess)
+        p @b
+        p win?(@b)
+        p attempt_used
+        p lost?
+      when 2
+        hint?
+      else
+        puts 'Please enter 1 or 2'
+      end
     end
+  end
 end
 
 #a = Cb.new
--- spec/codbreakers_spec.rb
+++ spec/codbreakers_spec.rb
@@ -1,100 +1,86 @@
 require "../lib/main.rb"
-describe Cb do 
+describe Cb do
   let(:subject) { Cb.new }
-  context 'When testing the #generator method' do 
-    it "returns an array size of 4" do 
-      ary_size = subject.generator.size 
-      expect(ary_size).to eq 4
-    end
+  context 'When testing the #generator method'
+  it 'returns an array size of 4' do
+    ary_size = subject.generator.size
+    expect(ary_size).to eq 4
+  end
 
-      it "returns array of integers" do
-        aray = subject.generator
-        integers_array = aray.select {|x| x.is_a? Integer}
-        expect(aray).to eq integers_array
-        end
+  it 'returns array of integers' do
+    aray = subject.generator
+    integers_array = aray.select { |x| x.is_a? Integer }
+    expect(aray).to eq integers_array
+  end
+  context 'When testing the #compare method'
+  it "returns all '+' when codes match" do
+    expect(subject.compare([1, 2, 3, 4], [1, 2, 3, 4])).to eq ['+', '+', '+', '+']
+  end
 
-      end
+  it "returns all ' ' when codes does not match" do
+    expect(subject.compare([1, 2, 3, 4], [5, 5, 5, 5])).to eq [' ', ' ', ' ', ' ']
+  end
 
-  context "When testing the #compare method"
+  it "returns all '-' when codes numbers are not in place" do
+    expect(subject.compare([1, 2, 3, 4], [4, 3, 2, 1])).to eq ['-', '-', '-', '-']
+  end
 
-    it "returns all '+' when codes match" do
+  it 'will check rest of numbers if match was found' do
+    expect(subject.compare([1, 2, 3, 4], [1, 3, 5, 1])).to eq ['+', '-', ' ', '-']
+  end
+  context 'when testing #gues method'
+  it 'returns array of integers' do
+    allow(subject).to receive(:gets) {'1234'}
+    expect(subject.guess).to eq [1, 2, 3, 4]
+  end
 
-      expect(subject.compare([1, 2, 3, 4], [1, 2, 3, 4])).to eq ["+", "+", "+", "+"]
-    end
-
-    it "returns all ' ' when codes does not match" do
-
-      expect(subject.compare([1, 2, 3, 4], [5, 5, 5, 5])).to eq [" ", " ", " ", " "]
-    end
-
-    it "returns all '-' when codes numbers are not in place" do
-
-      expect(subject.compare([1, 2, 3, 4], [4, 3, 2, 1])).to eq ["-", "-", "-", "-"]
-    end
-
-    it "will check rest of numbers if match was found" do
-
-      expect(subject.compare([1,2,3,4], [1,3,5,1])).to eq ["+", "-", " ", "-"]
-    end
-
-  context "when testing #gues method"
-
-    it "returns array of integers" do
-
-      allow(subject).to receive(:gets) {"1234"}  
-      expect(subject.guess).to eq [1, 2, 3, 4]
-    end
-
-  context "When testing #attempt_used method"
-  it "decreases attempts by 1 when used" do
-
+  context 'When testing #attempt_used method'
+  it 'decreases attempts by 1 when used' do
     allow(subject).to receive(:@attempts) { 5 }
     expect(subject.attempt_used).to eq 4
   end
 
-  context "when testing #lost? method"
+  context 'when testing #lost? method'
 
-  it "returns number of attempts left when not lost" do
+  it 'returns number of attempts left when not lost' do
     allow(subject).to receive(:@attempts) { 5 }
     expect { subject.lost? }.to output("Number of attempts left: 5\n").to_stdout
   end
 
-  it "calls #lost methot when attempts are 0" do
-#    allow(subject).to receive(:@attempts) { 0 }
+  it 'calls #lost methot when attempts are 0' do
     subject.instance_variable_set(:@attempts, 0)
     expect(subject).to receive(:lost)
     subject.lost?
   end
 
-context "when testing #win? method"
-  it "tests if #win method called on full match" do
+  context 'when testing #win? method'
+  it 'tests if #win method called on full match' do
     expect(subject).to receive(:win)
-    subject.win?(["+", "+", "+", "+"])
+    subject.win?(['+', '+', '+', '+'])
   end
 
-  it "tests that #win method is not called on non-full match" do
+  it 'tests that #win method is not called on non-full match' do
     expect(subject).not_to receive(:win)
-    subject.win?(["+", "+", "-", " "])
+    subject.win?(['+', '+', '-', ' '])
   end
 
-  context "when testing #hint? method"
-    it "checks that #hint method is called if user have hints" do
-      subject.instance_variable_set(:@hint_available, true)
-      expect(subject).to receive(:hint)
-      subject.hint?
-    end
+  context 'when testing #hint? method'
+  it 'checks that #hint method is called if user have hints' do
+    subject.instance_variable_set(:@hint_available, true)
+    expect(subject).to receive(:hint)
+    subject.hint?
+  end
 
-    it "checks that #hint method is not called if user don't have hints" do
-      subject.instance_variable_set(:@hint_available, false)
-      expect(subject).not_to receive(:hint)
-      subject.hint?
-    end
+  it "checks that #hint method is not called if user don't have hints" do
+    subject.instance_variable_set(:@hint_available, false)
+    expect(subject).not_to receive(:hint)
+    subject.hint?
+  end
 
   context "when testing #hint method"
-    
-    it "check that hint outputs correct value " do
-      expect { subject.hint([1, 2, 3, 4], ["+", "+", "+", "-"]) }.to output(/Number 4 is on position 4/).to_stdout
-    end
+  it 'check that hint outputs correct value' do
+    expect { subject.hint([1, 2, 3, 4], ["+", "+", "+", "-"]) }.to output(/Number 4 is on position 4/).to_stdout
+  end
 
 #    context "when testing #new_game method"
 #
@@ -105,16 +91,16 @@ context "when testing #win? method"
 #     subject.new_game
 #    end
 
-  context "When testing #win class"
-  it "test reset game" do
-    allow(subject).to receive(:gets) {"1"}
+  context 'When testing #win class'
+  it 'test reset game' do
+    allow(subject).to receive(:gets) {'1'}
     expect(subject).to receive(:new_game).once
     subject.win
   end
 
-  context "When testing #lost class"
+  context 'When testing #lost class'
   it do
-    allow(subject).to receive(:gets) {"1"}
+    allow(subject).to receive(:gets) {'1'}
     expect(subject).to receive(:new_game).once
     subject.win
   end
