f3283232efa0c677ebae3341a840cd5bd14d8707
Yevhen Tokarenko <ytokisd@gmail.com>
2018-06-17 16:36:38 +0300

    fix typo

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,5 +1,5 @@
 require_relative 'codebreaker/version.rb'
-require_relative 'codebreaker/game,rb'
+require_relative 'codebreaker/game.rb'
 require_relative 'codebreaker/menu.rb'
 require_relative 'codebreaker/data_manager.rb'
 require_relative 'codebreaker/processor.rb'
